bf37639d83d368792f91a1c8531ff3650d2722bc
dsgrafiniert <ds@grafiniert.de>
2017-03-10 19:32:12 +0100

    Update seafile.conf.sh
    Added mysql data

--- seafile.conf.sh
+++ seafile.conf.sh
@@ -14,4 +14,12 @@ max_upload_size=$MAX_UPLOAD
 
 # Set maximum download directory size to 200M.
 max_download_dir_size=$MAX_DOWNLOAD_DIR
+
+[database]
+type=mysql
+host=db
+user=${DB_ENV_MYSQL_USER}
+password=${DB_ENV_MYSQL_PASSWORD}
+db_name=seafile-db
+CONNECTION_CHARSET=utf8
 EOF
